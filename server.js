80bb84d404137a592c841c9247bfdc0333ef160a
bookair17 <kyoji-exterior@goo.jp>
2014-05-06 02:38:08 +0900

    update for deploy on heroku

--- server.js
+++ server.js
@@ -62,7 +62,8 @@ module.exports = app;
 
 var debug = require('debug')('my-application');
 //var app = require('../server');
-app.set('port', process.env.PORT || 5000);
+app.set('port', Number(process.env.PORT || 5000));
+
 var server = app.listen(app.get('port'), function() {
     debug('Express server listening on port ' + server.address().port);
 });
